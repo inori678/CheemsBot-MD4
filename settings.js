d87cce7ff03dcc8967edd9cdef579ae13dab3364
inori678 <110203716+inori678@users.noreply.github.com>
2022-11-15 15:41:55 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,8 +54,8 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['9660540637980'] //ur owner number
-global.ownername = "ABDUL RAHMAN🐼🍷 ٫" //ur owner name
+global.vcardowner = ['01033499817'] //ur owner number
+global.ownername = " 𝐈𝐍𝐎𝐑𝐈 ٫" //ur owner name
 global.ytname = "YT: ABDUL RAHMAN" //ur yt chanel name
 global.socialm = "GitHub: ABDUL RAHMAN" //ur github or insta name
 global.location = "Egypt, Cairo, Aizawl" //ur location
